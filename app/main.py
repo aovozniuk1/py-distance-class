435d64a2509c3e15adf06fe6cdc8660296c436e5
avozniuk <a.o.vozniuk@gmail.com>
2025-09-08 15:38:46 +0300

    solve

--- app/main.py
+++ app/main.py
@@ -1,3 +1,75 @@
+from typing import Any
+
+
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km):
+        self.km = km
+
+    def __str__(self):
+        return f"Distance: {self.km} kilometers."
+
+    def __repr__(self):
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: Any) -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        if isinstance(other, int):
+            return Distance(self.km + other)
+        return NotImplemented
+
+    def __iadd__(self, other: Any) -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(self.km + other.km)
+        if isinstance(other, int):
+            return Distance(self.km + other)
+        return NotImplemented
+
+    def __mul__(self, other: Any) -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(self.km * other.km)
+        if isinstance(other, int):
+            return Distance(self.km * other)
+        return NotImplemented
+
+    def __truediv__(self, other: Any) -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(self.km / other.km)
+        if isinstance(other, int):
+            return Distance(self.km / other)
+        return NotImplemented
+
+    def __lt__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km < other.km
+        if isinstance(other, int):
+            return self.km < other
+        return NotImplemented
+
+    def __le__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km <= other.km
+        if isinstance(other, int):
+            return self.km <= other
+        return NotImplemented
+
+    def __gt__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km > other.km
+        if isinstance(other, int):
+            return self.km > other
+        return NotImplemented
+
+    def __ge__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km >= other.km
+        if isinstance(other, int):
+            return self.km >= other
+        return NotImplemented
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, Distance):
+            return self.km == other.km
+        if isinstance(other, int):
+            return self.km == other
+        return NotImplemented
